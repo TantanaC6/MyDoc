a0d1c61ec7fde1c7314573babdbc15ffa59e0966
Nahedh-BenAbbes <nahedhabs@gmail.com>
2020-10-27 17:02:49 +0100

    Doctor login

--- Backend/dataBase/index.js
+++ Backend/dataBase/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mydoc', 'root', '123456', {
+const sequelize = new Sequelize('mydoc', 'root', '12345', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
