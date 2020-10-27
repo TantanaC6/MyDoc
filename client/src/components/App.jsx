e542e399d0e5cebf5f011ab89e8a24b12244de53
fejjari-oussama <oussamafejjari69@gmail.com>
2020-10-27 14:35:07 +0100

    Basic structr basic component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import DoctorHome from "./DoctorsComponents/DoctorHome.jsx"
 import PatientHome from "./PatientComponents/PatientHome.jsx"
-
+import Profile from "./PatientComponents/Profile.jsx"
+// import ConfirmAppointment from "./PatientComponents/ConfirmAppointment.jsx"
 class App extends React.Component {
   constructor() {
     super();
@@ -9,6 +10,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        
+        <Profile/>
+        {/* <ConfirmAppointment/> */}
         <DoctorHome />
         <PatientHome />
       </div>
